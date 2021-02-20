f554fc3810ea8cb4b72ef75ee9bb313dd17b28b6
Michael Navarro Ariza <rotpar@gmail.com>
2021-02-19 21:58:09 -0500

    Class is created to generate prime numbers
    Class is created to generate prime numbers

---
+++ Masiv.Printer.Refactor/GeneratePrimeNumber.cs
@@ -0,0 +1,55 @@
+﻿namespace Masiv.Printer.Refactor
+{
+    public class GeneratePrimeNumber
+    {
+        public int QuantityToGenerate { get; set; }
+
+        public int[] NumbersGenerated { get; set; }
+
+        public GeneratePrimeNumber(int quantityToGenerate)
+        {
+            this.QuantityToGenerate = quantityToGenerate;
+        }
+
+        public void Generate()
+        {
+            int K = 1;
+            int M = this.QuantityToGenerate;
+            int SQUARE = 9;
+            int J = 1;
+            int ORD = 2;
+            int ORDMAX = 30;
+            int[] MULT = new int[ORDMAX + 1];
+            int[] P = new int[M + 1];
+            int N = 0;
+            P[1] = 2;
+            bool JPRIME;
+            while (K < M)
+            {
+                do
+                {
+                    J += 2;
+                    if (J == SQUARE)
+                    {
+                        ORD++;
+                        SQUARE = P[ORD] * P[ORD];
+                        MULT[ORD - 1] = J;
+                    }
+                    N = 2;
+                    JPRIME = true;
+                    while (N < ORD && JPRIME)
+                    {
+                        while (MULT[N] < J)
+                            MULT[N] += P[N] + P[N];
+                        if (MULT[N] == J)
+                            JPRIME = false;
+                        N++;
+                    }
+                } while (!JPRIME);
+                K++;
+                P[K] = J;
+            }
+            this.NumbersGenerated = P;
+        }
+    }
+}
--- Masiv.Printer.Refactor/Program.cs
+++ Masiv.Printer.Refactor/Program.cs
@@ -6,65 +6,28 @@ namespace Masiv.Printer.Refactor
     {
         static void Main(string[] args)
         {
-            int M = 1000;
             int RR = 50;
             int CC = 4;
-            int ORDMAX = 30;
-            int[] P = new int[M + 1];
             int PAGENUMBER;
             int PAGEOFFSET;
             int ROWOFFSET;
             int C;
-            int J;
-            int K;
-            bool JPRIME;
-            int ORD;
-            int SQUARE;
-            int N = 0;
-            int[] MULT = new int[ORDMAX + 1];
-            J = 1;
-            K = 1;
-            P[1] = 2;
-            ORD = 2;
-            SQUARE = 9;
-            while (K < M)
-            {
-                do
-                {
-                    J += 2;
-                    if (J == SQUARE)
-                    {
-                        ORD++;
-                        SQUARE = P[ORD] * P[ORD];
-                        MULT[ORD - 1] = J;
-                    }
-                    N = 2;
-                    JPRIME = true;
-                    while (N < ORD && JPRIME)
-                    {
-                        while (MULT[N] < J)
-                            MULT[N] += P[N] + P[N];
-                        if (MULT[N] == J)
-                            JPRIME = false;
-                        N++;
-                    }
-                } while (!JPRIME);
-                K++;
-                P[K] = J;
-            }
+            GeneratePrimeNumber generatePrimeNumber = new GeneratePrimeNumber(1000);
+            generatePrimeNumber.Generate();
+            int[] P = generatePrimeNumber.NumbersGenerated;
             PAGENUMBER = 1;
             PAGEOFFSET = 1;
-            while (PAGEOFFSET <= M)
+            while (PAGEOFFSET <= generatePrimeNumber.QuantityToGenerate)
             {
                 Console.Write("The First ");
-                Console.Write(M.ToString());
+                Console.Write(generatePrimeNumber.QuantityToGenerate.ToString());
                 Console.Write(" Prime Numbers === Page ");
                 Console.Write(PAGENUMBER.ToString());
                 Console.WriteLine("\n");
                 for (ROWOFFSET = PAGEOFFSET; ROWOFFSET <= PAGEOFFSET + RR - 1; ROWOFFSET++)
                 {
                     for (C = 0; C <= CC - 1; C++)
-                        if (ROWOFFSET + C * RR <= M)
+                        if (ROWOFFSET + C * RR <= generatePrimeNumber.QuantityToGenerate)
                             Console.Write(P[ROWOFFSET + C * RR].ToString().PadLeft(10));
                     Console.WriteLine();
                 }
